3f4e166a72decbd34952856ced05fa8b4730c2d1
Tom Roscow <TomRoscow@users.noreply.github.com>
2024-03-16 19:13:50 +0000

    FIle for manually testing visualisation from command line before generation algorithm is in place

---
+++ src/temp.py
@@ -0,0 +1,9 @@
+from visualisation import draw_maze
+import argparse
+
+if __name__ == "__main__":
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("--tile_exits")
+    #args = parser.parse_args()
+
+    draw_maze([["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"]])
