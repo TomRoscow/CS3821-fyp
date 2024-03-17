2f780420ee093ac4e73a5fa00737addcf05596b2
Tom Roscow <TomRoscow@users.noreply.github.com>
2024-03-17 16:57:08 +0000

    Implements breadth-first search of maze graph

---
+++ src/graph_search/breadth_first.py
@@ -0,0 +1,15 @@
+def bfs(graph, start, goal):
+    queue = [(start, [start])]  # Queue of (vertex, path) tuples
+    visited = set()
+
+    while queue:
+        current, path = queue.pop(0)  # Dequeue the next vertex and path
+        if current == goal:
+            return path  # Return path to goal
+
+        for neighbour in graph.get(current, []):
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append((neighbour, path + [neighbour]))
+
+    return None  # Goal not reachable
--- src/temp.py
+++ src/temp.py
@@ -1,5 +1,7 @@
+import math
 from visualisation import draw_maze
 from graph import create_maze_graph
+from graph_search.breadth_first import bfs
 import argparse
 
 if __name__ == "__main__":
@@ -9,6 +11,16 @@ if __name__ == "__main__":
 
     # draw_maze([["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"], ["north"]])
 
-    flattened_exits, graph = create_maze_graph(5)
+    size = 10
+    flattened_exits, graph = create_maze_graph(size)
 
     draw_maze(flattened_exits, block=True)
+
+    
+    path = bfs(graph, (size-1, 0), (0, size-1))
+    if path:
+        print(f"Shortest path from character to reward: {path}")
+    else:
+        print(f"No path found from character to reward.")
+
+
